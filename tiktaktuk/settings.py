743da9fed740fe34631980bf583bf150ed36d65b
elcipeng <tamajustin322@gmail.com>
2026-05-12 01:05:01 +0700

    setup cloud database railway

--- tiktaktuk/settings.py
+++ tiktaktuk/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,10 +57,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tiktaktuk.wsgi.application'
 
+# Ganti bagian DATABASES kamu dengan ini:
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
